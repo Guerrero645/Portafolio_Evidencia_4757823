aaef6d5d3f30b983b3316abfe924f271a5dc6aa8
Guerrero645 <106697993+Guerrero645@users.noreply.github.com>
2023-06-08 20:53:02 -0600

    Hola Mundo en C

---
+++ hola.cpp
@@ -0,0 +1,13 @@
+////OSWALDO ISAI DE LA TORRE GUERRERO////4757823////
+
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    printf( "Hola mundo." );
+
+    getch(); /* Pausa */
+
+    return 0;
+}
